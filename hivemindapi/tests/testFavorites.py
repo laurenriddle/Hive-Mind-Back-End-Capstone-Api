eb053e68054810cee243350578db8110373adf8e
Lauren Riddle <lari7132@gmail.com>
2020-03-18 21:07:45 -0500

    added edit to interview testing

--- hivemindapi/tests/testFavorites.py
+++ hivemindapi/tests/testFavorites.py
@@ -80,6 +80,55 @@ class TestFavorites(TestCase):
         # queries the table to make sure the object we just added is in there by checking one of the properties. In this case, merchant_name.
         self.assertEqual(Favorite.objects.get().interview_id, 1)
 
+    def test_delete_payment_type(self):
+        self.industry = Industry.objects.create(
+            industry="Publishing"
+        )
+
+        # define a company
+        self.company = Company.objects.create(
+            name="Ingram Content Group",
+            industry_id=1,
+            city="Nashville",
+        )
+        # define a interview
+        self.interview = Interview.objects.create(
+            company_id= 1,
+            offer=False,
+            position="Software Developer I",
+            date="2020-11-11",
+            review="Went Well",
+            advice="Know OOP Fundamentals",
+            interview_type="First Interview",
+            in_person=True,
+            code_challege=True,
+            applicant_id=1
+        
+            )
+
+        # define a favorite to be POSTed to the DB
+        new_favorite = Favorite.objects.create(
+            applicant_id= 1,
+            interview_id= 1
+        )
+        
+         
+        # perform a delete on the object. Note: you have to reverse to favorite-detail sinces you are only targeting one object and not the whole list
+        response = self.client.delete(reverse('favorite-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
+ 
+        # Check that the response is 204 OK.
+        self.assertEqual(response.status_code, 204)
+
+        # Now we can grab all the favorites from the db. In this case there should be 0
+        response = self.client.get(reverse('favorite-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # test that the length of the response is 0, which means the delete was successful
+        self.assertEqual(len(response.data), 0)
+
+
     def test_get_favorites(self):
          # define an industry
         self.industry = Industry.objects.create(
--- hivemindapi/tests/testInterviews.py
+++ hivemindapi/tests/testInterviews.py
@@ -75,6 +75,48 @@ class TestInterviews(TestCase):
         # queries the table to make sure the object we just added is in there by checking one of the properties. In this case, merchant_name.
         self.assertEqual(Interview.objects.get().review, "Went Well")
 
+    def test_delete_payment_type(self):
+          # define an industry
+        self.industry = Industry.objects.create(
+            industry="Publishing"
+        )
+
+        # define a favorite
+        self.company = Company.objects.create(
+            name="Ingram Content Group",
+            industry_id=1,
+            city="Nashville",
+        )
+        # define a interview
+        new_interview = Interview.objects.create(
+            company_id= 1,
+            offer=False,
+            position="Software Developer I",
+            date="2020-11-11",
+            review="Went Well",
+            advice="Know OOP Fundamentals",
+            interview_type="First Interview",
+            in_person=True,
+            code_challege=True,
+            applicant_id=1
+        
+            )
+         
+        # perform a delete on the object. Note: you have to reverse to interview-detail sinces you are only targeting one object and not the whole list
+        response = self.client.delete(reverse('interview-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
+ 
+        # Check that the response is 204 OK.
+        self.assertEqual(response.status_code, 204)
+
+        # Now we can grab all the interviews from the db. In this case there should be 0
+        response = self.client.get(reverse('interview-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # test that the length of the response is 0, which means the delete was successful
+        self.assertEqual(len(response.data), 0)
+
     def test_get_favorites(self):
          # define an industry
         self.industry = Industry.objects.create(
@@ -178,6 +220,69 @@ class TestInterviews(TestCase):
         # Finally, test the actual rendered content as the client would receive it.
         # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
         self.assertIn(new_interview2.review.encode(), response.content)
+    
+    def test_edit_interview(self):
+        """TEST for update method on interview view"""
+        # define an industry
+        self.industry = Industry.objects.create(
+            industry="Publishing"
+        )
+
+        # define a company
+        self.company = Company.objects.create(
+            name="Ingram Content Group",
+            industry_id=1,
+            city="Nashville",
+        )
+        # define a interview
+        new_interview1 = Interview.objects.create(
+            company_id= 1,
+            offer=False,
+            position="Software Developer I",
+            date="2020-11-11",
+            review="Went Well",
+            advice="Know OOP Fundamentals",
+            interview_type="First Interview",
+            in_person=True,
+            code_challege=True,
+            applicant_id=1
+        
+        )
+        updated_interview = {
+            "company_id": 1,
+            "offer":False,
+            "position":"Software Developer I",
+            "date":"2020-11-11",
+            "review":"Went Great!",
+            "advice":"Know OOP Fundamentals",
+            "interview_type":"First Interview",
+            "in_person":True,
+            "code_challege":True,
+            "applicant_id":1
+        
+        }
+
+
+        # Update new_product
+        response = self.client.put(
+            reverse('interview-detail', kwargs={'pk': 1}), 
+            updated_interview,
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Token ' + str(self.token)
+        )
+
+        # Assert that the put returns the expected 204 status
+        self.assertEqual(response.status_code, 204)
+
+        # Get the order again, it should now be the updated order.
+        response = self.client.get(
+            reverse('interview-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token)
+        )
+        
+        # Assert that the product name is now self.payment_type_two.id
+        self.assertEqual(response.data["review"], "Went Great!")
+
+
 
         
 
